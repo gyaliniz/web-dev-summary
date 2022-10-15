c138efe60d2494de442b7811a5748113a6006677
Gokhan <gokhanyaliniz@gmail.com>
2022-10-16 01:02:46 +0300

    dom manipulation example

---
+++ js/10_dom_manipulation/index.js
@@ -0,0 +1,5 @@
+document.getElementById("info").innerHTML = "You just changed html file using JS"
+
+function getRandomValue () {
+   document.getElementById("randomValueResult").innerHTML = `${Math.random().toFixed(2)}`;
+}
